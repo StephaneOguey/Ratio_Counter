422bdd296c33854df9071869444e7c880a8bb22a
StephaneOguey <stephane.oguey@rpn.ch>
2017-12-07 15:21:26 +0100

    Creation demande insertion nombre de kill

--- Ratio_Counter/Program.cs
+++ Ratio_Counter/Program.cs
@@ -10,10 +10,23 @@ namespace Ratio_Counter
     {
         static void Main(string[] args)
         {
+            char cNouvPartie;
+
+
             Console.WriteLine("Entrez votre Pseudo :");
             string strPseudo = Console.ReadLine();
 
             Console.WriteLine("Avez-vous jouez une nouvelle partie [o/n]");
+            cNouvPartie = Console.ReadKey().KeyChar;
+
+            if (cNouvPartie == 'o')
+            {
+                Console.WriteLine("Combien de kill avez-vous fait ???");
+                string strNbKillMatch = Console.ReadLine();
+                double dblNbKillMatch = Convert.ToDouble(strNbKillMatch);
+
+
+            }
         }
     }
 }
