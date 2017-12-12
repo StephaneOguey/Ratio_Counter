a9e6b4f765d75d7e6af5d995afd5fbfc98a85752
StephaneOguey <stephane.oguey@rpn.ch>
2017-12-12 13:58:39 +0100

    Creation de la fonction de selection de programme avec un switch

--- Ratio_Counter/Program.cs
+++ Ratio_Counter/Program.cs
@@ -10,7 +10,27 @@ namespace Ratio_Counter
     {
         static void Main(string[] args)
         {
+            char cChoixProg;
 
+            Console.WriteLine("Pour calculer votre ratio Kill / Mort, tapez [1] !");
+            Console.WriteLine("Pour calculer votre ratio de Partie Gagnée / Partie Perdu, tapez [2] ");
+
+            cChoixProg = Console.ReadKey().KeyChar;
+
+            switch (cChoixProg)
+            {
+                case '1':
+                    Ratio_Kill_Mort();
+                    break;
+
+                case '2':
+
+                    break;
+
+                default:
+                    
+                    break;
+            }
         }
         static double Ratio_Kill_Mort()
         {
@@ -31,6 +51,7 @@ namespace Ratio_Counter
 
 
             }
+            return ();
         }
     }
 }
