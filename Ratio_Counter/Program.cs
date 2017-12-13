b09515d932886e62df753dc4fc555f167438b1a0
StephaneOguey <stephane.oguey@rpn.ch>
2017-12-13 08:52:47 +0100

    Deplacement des precedantes fonctions dans le switch et changement de double à int pour certaine variable

--- Ratio_Counter/Program.cs
+++ Ratio_Counter/Program.cs
@@ -10,8 +10,17 @@ namespace Ratio_Counter
     {
         static void Main(string[] args)
         {
+            string strPseudo = "Anonyme";
+            double dblRatio_Gagnée_Perdue = 0;
+            int iNbGagnée = 0;
+            int iNbPerdue = 0;
+            double dblRatio_Kill_Mort = 0;
+            int iNbKill = 0;
+            int iNbMort = 0;
             char cChoixProg;
             char cQuitteProg = 'n';
+
+
             do
             {
                 Console.WriteLine("Pour calculer votre ratio Kill / Mort, tapez [1] !");
@@ -24,15 +33,136 @@ namespace Ratio_Counter
                 switch (cChoixProg)
                 {
                     case '1':
-                        Ratio_Kill_Mort();
+                        char cNouvPartie;
+                        char cQuitteFonc = 'n';
+
+                        Console.WriteLine("\nAvez-vous jouez une nouvelle partie [o/n]");
+                        cNouvPartie = Console.ReadKey().KeyChar;
+
+                        if (cNouvPartie == 'o')
+                        {
+                            Console.WriteLine("\nCombien de kill avez-vous fait ???");
+                            string strNbKillMatch = Console.ReadLine();
+                            int iNbKillMatch = Convert.ToInt32(strNbKillMatch);
+                            iNbKill += iNbKillMatch;
+
+                            Console.WriteLine("\nCombien de mort avez-vous fait ???");
+                            string strNbMortMatch = Console.ReadLine();
+                            int iNbMortMatch = Convert.ToInt32(strNbMortMatch);
+                            iNbMort += iNbMortMatch;
+
+                            dblRatio_Kill_Mort = (double) iNbKill / iNbMort;
+
+                            Console.Clear();
+                        }
+                        do
+                        {
+                            Console.WriteLine("\nVotre Ratio de la journée est de {0}", dblRatio_Kill_Mort);
+                            Console.WriteLine("\nPressez une touche pour continuer...");
+                            Console.ReadKey();
+
+                            Console.WriteLine("Avez-vous jouez une nouvelle partie [o/n]");
+                            cNouvPartie = Console.ReadKey().KeyChar;
+
+                            if (cNouvPartie == 'o')
+                            {
+                                Console.WriteLine("\nCombien de kill avez-vous fait ???");
+                                string strNbKillMatch = Console.ReadLine();
+                                int iNbKillMatch = Convert.ToInt32(strNbKillMatch);
+                                iNbKill += iNbKillMatch;
+
+                                Console.WriteLine("\nCombien de mort avez-vous fait ???");
+                                string strNbMortMatch = Console.ReadLine();
+                                int iNbMortMatch = Convert.ToInt32(strNbMortMatch);
+                                iNbMort += iNbMortMatch;
+
+                                dblRatio_Kill_Mort = (double) iNbKill / iNbMort;
+
+                                Console.Clear();
+                            }
+                            if (cNouvPartie == 'n')
+                            {
+                                Console.WriteLine("\nVoulez-vous quitter le programme de calcul de Ratio Kill/Mort [o/n] ???");
+                                cQuitteFonc = Console.ReadKey().KeyChar;
+                            }
+                            Console.Clear();
+                        }
+                        while (!(cQuitteFonc == 'o'));
                         break;
 
+
+
                     case '2':
-                        Ratio_Manche_Gagnée_Perdue();
+                        cNouvPartie = 'n';
+                        cQuitteFonc = 'n';
+
+
+                        Console.WriteLine("\nEntrez votre Pseudo :");
+                        strPseudo = Console.ReadLine();
+
+                        Console.WriteLine("\nAvez-vous jouez une nouvelle partie [o/n]");
+                        cNouvPartie = Console.ReadKey().KeyChar;
+
+                        if (cNouvPartie == 'o')
+                        {
+                            Console.WriteLine("\nCombien de Manche Gagnée avez-vous fait ???");
+                            string strNbGagnéeMatch = Console.ReadLine();
+                            int iNbGagnéeMatch = Convert.ToInt32(strNbGagnéeMatch);
+                            iNbGagnée += iNbGagnéeMatch;
+
+                            Console.WriteLine("\nCombien de Manche Perdue avez-vous fait ???");
+                            string strNbPerdueMatch = Console.ReadLine();
+                            int iNbPerdueMatch = Convert.ToInt32(strNbPerdueMatch);
+                            iNbPerdue += iNbPerdueMatch;
+
+                            dblRatio_Gagnée_Perdue = (double) iNbGagnée / iNbPerdue;
+
+                            Console.Clear();
+                        }
+                        do
+                        {
+                            Console.WriteLine("\nVotre Ratio de la journée est de {0}", dblRatio_Gagnée_Perdue);
+                            Console.WriteLine("\nPressez une touche pour continuer...");
+                            Console.ReadKey();
+
+                            Console.WriteLine("\nAvez-vous jouez une nouvelle partie [o/n]");
+                            cNouvPartie = Console.ReadKey().KeyChar;
+
+                            if (cNouvPartie == 'o')
+                            {
+                                Console.WriteLine("\nCombien de Manche Gagnée avez-vous fait ???");
+                                string strNbGagnéeMatch = Console.ReadLine();
+                                int iNbGagnéeMatch = Convert.ToInt32(strNbGagnéeMatch);
+                                iNbGagnée += iNbGagnéeMatch;
+
+                                Console.WriteLine("\nCombien de Manche Perdue avez-vous fait ???");
+                                string strNbPerdueMatch = Console.ReadLine();
+                                int iNbPerdueMatch = Convert.ToInt32(strNbPerdueMatch);
+                                iNbPerdue += iNbPerdueMatch;
+
+                                dblRatio_Gagnée_Perdue = (double)iNbGagnée / iNbPerdue;
+
+                                Console.Clear();
+                            }
+                            if (cNouvPartie == 'n')
+                            {
+                                Console.WriteLine("\nVoulez-vous quitter le programme de calcul de Ratio Gagnée/Perdue [o/n] ???");
+                                cQuitteFonc = Console.ReadKey().KeyChar;
+                            }
+                            Console.Clear();
+                        }
+                        while (!(cQuitteFonc == 'o'));
                         break;
 
                     case '3':
-                        Recap();
+                        Console.WriteLine("\n=======================================================");
+                        Console.WriteLine("Votre Ratio de Kill / Mort est de {0}", dblRatio_Kill_Mort);
+                        Console.WriteLine("=======================================================");
+                        Console.WriteLine("Votre Ratio Manche Gagnée / Manche Perdue est de {0}", dblRatio_Gagnée_Perdue);
+                        Console.WriteLine("=======================================================");
+                        Console.WriteLine("\nPressez une touche pour continuer...");
+                        Console.ReadKey();
+                        Console.Clear();
                         break;
                     case '4':
                         Console.WriteLine("\nVoulez-vous vraiment quitter le programme [o/n] ???");
@@ -50,149 +180,6 @@ namespace Ratio_Counter
             }
             while (!(cQuitteProg == 0));
         }
-        static double Ratio_Kill_Mort()
-        {
-            char cNouvPartie;
-            char cQuitteFonc = 'n';
-            double dblRatio_Kill_Mort = 0;
-            double dblNbKill = 0;
-            double dblNbMort = 0;
 
-            Console.WriteLine("\nEntrez votre Pseudo :");
-            string strPseudo = Console.ReadLine();
-
-            Console.WriteLine("\nAvez-vous jouez une nouvelle partie [o/n]");
-            cNouvPartie = Console.ReadKey().KeyChar;
-
-            if (cNouvPartie == 'o')
-            {
-                Console.WriteLine("\nCombien de kill avez-vous fait ???");
-                string strNbKillMatch = Console.ReadLine();
-                double dblNbKillMatch = Convert.ToDouble(strNbKillMatch);
-                dblNbKill += dblNbKillMatch;
-
-                Console.WriteLine("\nCombien de mort avez-vous fait ???");
-                string strNbMortMatch = Console.ReadLine();
-                double dblNbMortMatch = Convert.ToDouble(strNbMortMatch);
-                dblNbMort += dblNbMortMatch;
-
-                dblRatio_Kill_Mort = dblNbKill / dblNbMort;
-
-                Console.Clear();
-            }
-            do
-            {
-                Console.WriteLine("\nVotre Ratio de la journée est de {0}", dblRatio_Kill_Mort);
-                Console.WriteLine("\nPressez une touche pour continuer...");
-                Console.ReadKey();
-
-                Console.WriteLine("Avez-vous jouez une nouvelle partie [o/n]");
-                cNouvPartie = Console.ReadKey().KeyChar;
-
-                if (cNouvPartie == 'o')
-                {
-                    Console.WriteLine("\nCombien de kill avez-vous fait ???");
-                    string strNbKillMatch = Console.ReadLine();
-                    double dblNbKillMatch = Convert.ToDouble(strNbKillMatch);
-                    dblNbKill += dblNbKillMatch;
-
-                    Console.WriteLine("\nCombien de mort avez-vous fait ???");
-                    string strNbMortMatch = Console.ReadLine();
-                    double dblNbMortMatch = Convert.ToDouble(strNbMortMatch);
-                    dblNbMort += dblNbMortMatch;
-
-                    dblRatio_Kill_Mort = dblNbKill / dblNbMort;
-
-                    Console.Clear();
-                }
-                if (cNouvPartie == 'n')
-                {
-                    Console.WriteLine("\nVoulez-vous quitter le programme de calcul de Ratio Kill/Mort [o/n] ???");
-                    cQuitteFonc = Console.ReadKey().KeyChar;
-                }
-                Console.Clear();
-            }
-            while (!(cQuitteFonc == 'o'));
-
-            return dblRatio_Kill_Mort;
-        }
-        static double Ratio_Manche_Gagnée_Perdue()
-        {
-            char cNouvPartie;
-            char cQuitteFonc = 'n';
-            double dblRatio_Gagnée_Perdue = 0;
-            double dblNbGagnée = 0;
-            double dblNbPerdue = 0;
-
-            Console.WriteLine("\nEntrez votre Pseudo :");
-            string strPseudo = Console.ReadLine();
-
-            Console.WriteLine("\nAvez-vous jouez une nouvelle partie [o/n]");
-            cNouvPartie = Console.ReadKey().KeyChar;
-
-            if (cNouvPartie == 'o')
-            {
-                Console.WriteLine("\nCombien de Manche Gagnée avez-vous fait ???");
-                string strNbGagnéeMatch = Console.ReadLine();
-                double dblNbGagnéeMatch = Convert.ToDouble(strNbGagnéeMatch);
-                dblNbGagnée += dblNbGagnéeMatch;
-
-                Console.WriteLine("\nCombien de Manche Perdue avez-vous fait ???");
-                string strNbPerdueMatch = Console.ReadLine();
-                double dblNbPerdueMatch = Convert.ToDouble(strNbPerdueMatch);
-                dblNbPerdue += dblNbPerdueMatch;
-
-                dblRatio_Gagnée_Perdue = dblNbGagnée / dblNbPerdue;
-
-                Console.Clear();
-            }
-            do
-            {
-                Console.WriteLine("\nVotre Ratio de la journée est de {0}", dblRatio_Gagnée_Perdue);
-                Console.WriteLine("\nPressez une touche pour continuer...");
-                Console.ReadKey();
-
-                Console.WriteLine("\nAvez-vous jouez une nouvelle partie [o/n]");
-                cNouvPartie = Console.ReadKey().KeyChar;
-
-                if (cNouvPartie == 'o')
-                {
-                    Console.WriteLine("\nCombien de Manche Gagnée avez-vous fait ???");
-                    string strNbGagnéeMatch = Console.ReadLine();
-                    double dblNbGagnéeMatch = Convert.ToDouble(strNbGagnéeMatch);
-                    dblNbGagnée += dblNbGagnéeMatch;
-
-                    Console.WriteLine("\nCombien de Manche Perdue avez-vous fait ???");
-                    string strNbPerdueMatch = Console.ReadLine();
-                    double dblNbPerdueMatch = Convert.ToDouble(strNbPerdueMatch);
-                    dblNbPerdue += dblNbPerdueMatch;
-
-                    dblRatio_Gagnée_Perdue = dblNbGagnée / dblNbPerdue;
-
-                    Console.Clear();
-                }
-                if (cNouvPartie == 'n')
-                {
-                    Console.WriteLine("\nVoulez-vous quitter le programme de calcul de Ratio Gagnée/Perdue [o/n] ???");
-                    cQuitteFonc = Console.ReadKey().KeyChar;
-                }
-                Console.Clear();
-            }
-            while (!(cQuitteFonc == 'o'));
-            return dblRatio_Gagnée_Perdue;
-        }
-        static double Recap()
-        {
-            Console.WriteLine("\n============================0==========================");
-            Console.WriteLine("Votre Ratio de Kill / Mort est de {0}",Ratio_Kill_Mort());
-            Console.WriteLine("=======================================================");
-            Console.WriteLine("Votre Ratio Manche Gagnée / Manche Perdue est de {0}",Ratio_Manche_Gagnée_Perdue());
-            Console.WriteLine("=======================================================");
-            Console.WriteLine("\nPressez une touche pour continuer...");
-            Console.ReadKey();
-            Console.Clear();
-
-            return (0);
-        }
     }
 }
